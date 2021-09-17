5cd6ab596b72d1fa3dacdfe4ca93eeb7ea4294d2
Ronald Papandrea <ronald@bai.network>
2021-09-17 19:31:54 +0800

    Added upgradable / proxy.

--- contracts/bai/BAI.sol
+++ contracts/bai/BAI.sol
@@ -2,46 +2,30 @@
 pragma solidity 0.8.6;
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableMapUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableSetUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/MathUpgradeable.sol";
+
+import "@uniswap/lib/contracts/libraries/TransferHelper.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@uniswap/lib/contracts/libraries/TransferHelper.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import "@openzeppelin/contracts/utils/math/Math.sol";
 
-enum Frequency {
-    UNKNOWN, //0
-    DAILY, // 1
-    WEEKLY, // 2
-    BIWEEKLY, // 3
-    MONTHLY, // 4
-    // FOR DEVELOPMENT ONLY
-    HOURLY, // 5
-    PERMINUTE //6
-}
+import "../interfaces/IBaiController.sol";
 
-interface IBaiController {
-    function config(Frequency frequency, uint256 amountPerTime) external;
-
-    function deposit(uint256 amount) external;
-
-    function withdraw(uint256 usdcAmount, uint256 btcAmount)
-        external
-        returns (bool);
-
-    function swap() external returns (bool);
-}
-
-contract BaiController is IBaiController, Context, Ownable, Pausable {
-    using SafeMath for uint256;
-    using Address for address;
-    using SafeERC20 for IERC20;
+contract BaiController is
+    IBaiController,
+    AccessControlUpgradeable,
+    PausableUpgradeable
+{
+    using SafeMathUpgradeable for uint256;
+    using AddressUpgradeable for address;
+    using SafeERC20Upgradeable for IERC20Upgradeable;
 
     struct Configuration {
         Frequency frequency;
@@ -56,10 +40,10 @@ contract BaiController is IBaiController, Context, Ownable, Pausable {
 
     IUniswapV2Router02 public immutable uniswapV2Router;
     // address public immutable uniswapV2Pair;
-    IERC20 public constant WBTC =
-        IERC20(0x577D296678535e4903D59A4C929B718e1D575e0A); // rinkeby // mainnet: 0x2260fac5e5542a773aa44fbcfedf7c193bc2c599 //kovan: 0xd3A691C852CDB01E281545A27064741F0B7f6825
-    IERC20 public constant USDC =
-        IERC20(0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b); // rinkeby // mainnet: 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48 //kovan: 0xb7a4F3E9097C08dA09517b5aB877F7a917224ede
+    IERC20Upgradeable public constant WBTC =
+        IERC20Upgradeable(0x577D296678535e4903D59A4C929B718e1D575e0A); // rinkeby // mainnet: 0x2260fac5e5542a773aa44fbcfedf7c193bc2c599 //kovan: 0xd3A691C852CDB01E281545A27064741F0B7f6825
+    IERC20Upgradeable public constant USDC =
+        IERC20Upgradeable(0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b); // rinkeby // mainnet: 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48 //kovan: 0xb7a4F3E9097C08dA09517b5aB877F7a917224ede
     address private constant UNISWAP_ROUTER =
         0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     address[] investors;
@@ -69,8 +53,6 @@ contract BaiController is IBaiController, Context, Ownable, Pausable {
     mapping(address => Tx[]) private transactions;
     mapping(address => uint256) private lastTraded;
     mapping(address => bool) public exists;
-    // mapping(address => uint256) private indexOfInvestors;
-    // uint256 private index = 1;
     uint256 public constant MIN_AMOUNT = 10**6 * 100; // min 100 USDC
     uint256 public constant DENOMINATOR = 1e4;
     bool swapInProgress = false;
@@ -104,14 +86,32 @@ contract BaiController is IBaiController, Context, Ownable, Pausable {
         IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(
             UNISWAP_ROUTER
         );
+        // uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory()).getPair(
+        //     address(USDC),
+        //     address(WBTC)
+        // );
 
+        // set the rest of the contract variables
         uniswapV2Router = _uniswapV2Router;
+    }
+
+    function initialize(address ownerAccount) public initializer whenNotPaused {
+        require(ownerAccount != address(0), "BAI::constructor:Zero address");
+
+        _setupRole(DEFAULT_ADMIN_ROLE, ownerAccount);
+
+        // Paused on launch
+        _pause();
+    }
+
+    function unpause() external onlyRole(DEFAULT_ADMIN_ROLE) whenPaused {
+        _unpause();
     }
 
     //to recieve ETH from uniswapV2Router when swaping
     receive() external payable {}
 
-    function kill() external onlyOwner {
+    function kill() external onlyRole(DEFAULT_ADMIN_ROLE) {
         uint256 balance = USDC.balanceOf(address(this));
         if (balance > 0) USDC.safeTransfer(_msgSender(), balance);
         balance = WBTC.balanceOf(address(this));
@@ -119,7 +119,10 @@ contract BaiController is IBaiController, Context, Ownable, Pausable {
         selfdestruct(payable(_msgSender()));
     }
 
-    function setMaxItemPerStep(uint8 step) external onlyOwner {
+    function setMaxItemPerStep(uint8 step)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
         maxItemsPerStep = step;
     }
 
@@ -186,6 +189,8 @@ contract BaiController is IBaiController, Context, Ownable, Pausable {
         );
         require(usdcBalance[_user] >= usdcAmount, "Insufficient USDC amount");
         require(btcBalance[_user] >= btcAmount, "Insufficient WBTC amount");
+        // require(USDC.balanceOf(address(this)) >= usdcBalance[_user], 'Insufficient USDC on BAI');
+        // require(WBTC.balanceOf(address(this)) >= btcBalance[_user], 'Insufficient WBTC on BAI');
 
         uint256 _allowanceOfUsdc = USDC.allowance(
             address(this),
@@ -309,7 +314,7 @@ contract BaiController is IBaiController, Context, Ownable, Pausable {
         uint256 amountOfBTC2 = results[results.length - 1];
 
         path = amountOfBTC2 > amountOfBTC1 ? path2 : path1;
-        amountsOut = Math.max(amountOfBTC1, amountOfBTC2);
+        amountsOut = MathUpgradeable.max(amountOfBTC1, amountOfBTC2);
     }
 
     function prepare()
@@ -440,7 +445,10 @@ contract BaiController is IBaiController, Context, Ownable, Pausable {
         Tx[] memory txs = transactions[user];
         if (txs.length > 0) {
             uint256 from = page == 0 ? 0 : (page - 1) * size;
-            uint256 to = Math.min((page == 0 ? 1 : page) * size, txs.length);
+            uint256 to = MathUpgradeable.min(
+                (page == 0 ? 1 : page) * size,
+                txs.length
+            );
             Tx[] memory infos = new Tx[]((to - from));
             for (uint256 i = 0; from < to; ++i) {
                 infos[i] = txs[from];
@@ -463,7 +471,10 @@ contract BaiController is IBaiController, Context, Ownable, Pausable {
             uint256 from = txs.length - 1 - (page == 0 ? 0 : (page - 1) * size);
             uint256 to = txs.length -
                 1 -
-                Math.min((page == 0 ? 1 : page) * size - 1, txs.length - 1);
+                MathUpgradeable.min(
+                    (page == 0 ? 1 : page) * size - 1,
+                    txs.length - 1
+                );
             uint256 resultSize = from - to + 1;
             Tx[] memory infos = new Tx[](resultSize);
             if (to == 0) {
@@ -502,7 +513,6 @@ contract BaiController is IBaiController, Context, Ownable, Pausable {
             ? 1 minutes
             : 0;
 
-        // require(totalSeconds > 0, 'Invalid frequency value');
         return totalSeconds;
     }
 
---
+++ contracts/interfaces/IBaiController.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+enum Frequency {
+    UNKNOWN, //0
+    DAILY, // 1
+    WEEKLY, // 2
+    BIWEEKLY, // 3
+    MONTHLY, // 4
+    // FOR DEVELOPMENT ONLY
+    HOURLY, // 5
+    PERMINUTE //6
+}
+
+interface IBaiController {
+    function config(Frequency frequency, uint256 amountPerTime) external;
+
+    function deposit(uint256 amount) external;
+
+    function withdraw(uint256 usdcAmount, uint256 btcAmount)
+        external
+        returns (bool);
+
+    function swap() external returns (bool);
+}
